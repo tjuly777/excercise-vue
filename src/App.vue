87ed28d657167c890a1624737521c411df908f48
tjuly777 <tjuly777@gmail.com>
2019-11-28 15:47:26 +0900

    doc: v-model、v-ifについてのコメント追記

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <p v-if="msg.length > 0">
-      {{msg}}
-    </p>
-    <p v-else>no text.</p>
+    <!--v-modelにバインディングしたデータ名を指定すると、値が連動 -->
     <input type="text" v-model="msg">
+    
+    <!--v-ifによるif文で、タグ単位の条件分岐が可能-->
+    <p v-if="msg.length > 0">{{msg}}</p>
+    <p v-else>no text.</p>
+    
   </div>
 </template>
 
