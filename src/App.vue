6dddb1d9b27d25a0093f363de8f4b2d9c4047105
tjuly777 <tjuly777@gmail.com>
2019-11-28 16:06:01 +0900

    feat: コンポーネントの宣言と使用を実践。

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    <!--v-modelにバインディングしたデータ名を指定すると、値が連動 -->
-    <input type="text" v-model="msg">
-    
-    <!--v-ifによるif文で、タグ単位の条件分岐が可能-->
-    <p v-if="msg.length > 0">{{msg}}</p>
-    <p v-else>no text.</p>
-    
-  </div>
+    <div>
+        <myheader></myheader>
+        
+        <!--v-modelにバインディングしたデータ名を指定すると、値が連動 -->
+        <input type="text" v-model="msg">
+        
+        <!--v-ifによるif文で、タグ単位の条件分岐が可能-->
+        <p v-if="msg.length > 0">{{msg}}</p>
+        <p v-else>no text.</p>
+        
+        <button @click="clear()">Clear!</button>
+    </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        msg: "It's a Sample."
-      }
+    import myheader from './components/myheader'
+    export default {
+        /*コンポーネントを宣言し、タグ名として指定すると展開。*/
+        components: {
+            myheader
+        },
+        
+        /*データバインディング*/
+        data() {
+            return {
+                msg: "It's a Sample."
+            }
+        },
+        /*メソッドの宣言*/
+        methods: {
+            clear() {
+                this.msg = ''
+            }
+        }
     }
-  }
 </script>
---
+++ src/components/myheader.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Here is header.</div>
+</template>
